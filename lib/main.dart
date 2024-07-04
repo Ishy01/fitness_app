77e8ce30db3857cd82c8a1b69f576bfe1b72a602
Ishy <irtettedzie554@gmail.com>
2024-07-04 13:07:08 +0000

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,28 @@ import 'package:fitness_app/screens/home/recipe_scree.dart';
 import 'package:fitness_app/screens/main_tab/main_screen.dart';
 import 'package:fitness_app/screens/on_boarding/started_view.dart';
 import 'package:fitness_app/screens/profile/profile_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 // import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyBQeedlqf-JW5s1OlLlvMKfGwkPgEAgQ-I",
+            authDomain: "fitness-server-4ff1e.firebaseapp.com",
+            projectId: "fitness-server-4ff1e",
+            storageBucket: "fitness-server-4ff1e.appspot.com",
+            messagingSenderId: "251012622167",
+            appId: "1:251012622167:web:45313dd191a5f3a88ec012"));
+  } else {
+    Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -39,7 +55,7 @@ class MyApp extends StatelessWidget {
         fontFamily: "Poppins",
         primaryColor: TextColor.primaryColor1,
       ),
-      home: MainScreen(),
+      home: StartedView(),
       routes: {
         '/home': (context) => HomeScreen(),
         '/profile': (context) => ProfileScreen(),
